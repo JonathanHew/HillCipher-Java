4745d3ed7fe61f584f46dfe5c1a309657f0a417c
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-17 22:48:18 +0100

    decrypt() not needed, removed irrelevant lines

--- HillCipher.java
+++ HillCipher.java
@@ -9,16 +9,13 @@ public class HillCipher {
             System.out.println("\n\n-----------Hill Cipher-----------");
             System.out.println("Please choose one of the following options:");
             System.out.println("1. Encrypt Text");
-            System.out.println("2. Decrypt Text");
-            System.out.println("3. Quit");
+            System.out.println("2. Quit");
 
             int input = scan.nextInt();
 
             if (input == 1) {
                 Encrypt();
             } else if (input == 2) {
-                Decrypt();
-            } else if (input == 3) {
                 System.out.println("Goodbye!");
                 break;
             } else {
@@ -31,7 +28,7 @@ public class HillCipher {
         Scanner scan = new Scanner(System.in);
         String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String answer = "";
-        String plaintext ="";
+        String plaintext = "";
         String key = "";
         ArrayList<Integer> keyArray = new ArrayList<>();
         ArrayList<Integer> textArray = new ArrayList<>();
@@ -39,87 +36,64 @@ public class HillCipher {
 
         System.out.println("You have entered Encrypt!");
 
-        //keep asking for plaintext until 4 letter word entered
-        while(true)
-        {
+        // keep asking for plaintext until 4 letter word entered
+        while (true) {
             System.out.println("Please enter plaintext to be encrypted. (4 letters) ");
             plaintext = scan.nextLine().toUpperCase();
 
-            if(plaintext.length() == 4)
-            {
+            if (plaintext.length() == 4) {
                 break;
-            }
-            else
-            {
+            } else {
                 System.out.println("Plaintext must be 4 letters only!");
             }
         }
 
-        //keep asking key until 4 letter word entered
-        while(true)
-        {
+        // keep asking key until 4 letter word entered
+        while (true) {
             System.out.println("Please enter key to be used in the encryption (4 letters) ");
             key = scan.nextLine().toUpperCase();
 
-            if(key.length() == 4)
-            {
+            if (key.length() == 4) {
                 break;
-            }
-            else
-            {
+            } else {
                 System.out.println("Key must be 4 letters only!");
             }
         }
 
-        //creating the plaintext matrix
-        for(int i=0;i<plaintext.length();i++)
-        {
+        // creating the plaintext matrix
+        for (int i = 0; i < plaintext.length(); i++) {
             char letter = plaintext.charAt(i);
 
-            for(int j=0;j<ALPHABET.length();j++)
-            {
-                if(ALPHABET.charAt(j) == letter)
-                {
+            for (int j = 0; j < ALPHABET.length(); j++) {
+                if (ALPHABET.charAt(j) == letter) {
                     textArray.add(j);
                 }
             }
         }
 
-        //creating the key matrix
-        for(int i=0;i<key.length();i++)
-        {
+        // creating the key matrix
+        for (int i = 0; i < key.length(); i++) {
             char letter = key.charAt(i);
 
-            for(int j=0;j<ALPHABET.length();j++)
-            {
-                if(ALPHABET.charAt(j) == letter)
-                {
+            for (int j = 0; j < ALPHABET.length(); j++) {
+                if (ALPHABET.charAt(j) == letter) {
                     keyArray.add(j);
                 }
             }
         }
 
-
-        //creating the answer matrix
+        // creating the answer matrix
         answerArray.clear();
-        answerArray.add((textArray.get(0)*keyArray.get(0))+ (textArray.get(1)*keyArray.get(2)) % 26);
-        answerArray.add((textArray.get(0)*keyArray.get(1)) + (textArray.get(1)*keyArray.get(3)) % 26);
-        answerArray.add((textArray.get(2)*keyArray.get(0)) + (textArray.get(3)*keyArray.get(2)) %26);
-        answerArray.add((textArray.get(2)*keyArray.get(1)) + (textArray.get(3)*keyArray.get(3)) %26);
+        answerArray.add((textArray.get(0) * keyArray.get(0)) + (textArray.get(1) * keyArray.get(2)) % 26);
+        answerArray.add((textArray.get(0) * keyArray.get(1)) + (textArray.get(1) * keyArray.get(3)) % 26);
+        answerArray.add((textArray.get(2) * keyArray.get(0)) + (textArray.get(3) * keyArray.get(2)) % 26);
+        answerArray.add((textArray.get(2) * keyArray.get(1)) + (textArray.get(3) * keyArray.get(3)) % 26);
 
-        //creating the encrytped string using the answer matrix
-        for(int i=0;i<answerArray.size();i++)
-        {
-            answer = answer+ALPHABET.charAt(answerArray.get(i));
+        // creating the encrytped string using the answer matrix
+        for (int i = 0; i < answerArray.size(); i++) {
+            answer = answer + ALPHABET.charAt(answerArray.get(i));
         }
 
-        System.out.println("Plaintext: " + plaintext + " Encrypted using Key: " +  key + " = " + answer);
+        System.out.println("Plaintext: " + plaintext + " Encrypted using Key: " + key + " = " + answer);
     }// end Encrypt()
-
-    public static void Decrypt() {
-        System.out.println("You have entered Decrypt!");
-        
-    }// end Decrypt()
-
-    
 }// end CaesarCipher
